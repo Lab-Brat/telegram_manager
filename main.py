c2f7f7f18ccab706cda9920e9207be169db709df
Lab-Brat <xooVoox@protonmail.com>
2022-04-06 10:26:44 +0300

    remove redundant lines

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ config.read('config.txt')
 api_id = int(config['USER']['api_id'])
 api_hash = config['USER']['api_hash']
 phone = config['USER']['phone']
-
-print(api_id, api_hash, phone)
-print(type(api_id), type(api_hash), type(phone))
-
-
 client = TelegramClient(phone, api_id, api_hash)
 
 async def main():
@@ -35,13 +30,8 @@ async def main():
                 groups.append(chat)
         except:
             continue
-    
-    # for chat in chats:
-    #     if chat.group == True:
-    #         groups.append(chat)
-    # pprint([t.title for t in groups])
-    print('Choose a group to scrape: ')
 
+    print('Choose a group to scrape: ')
     for i, group in enumerate(groups):
         print(str(i) + '- ' + group.title)
     gr_index = input("Enter a number: ")
@@ -60,23 +50,7 @@ async def main():
     print('Fetching Members...')
     all_members = []
     all_members = await client.get_participants(target_group)
-
-    print('Saving in file...')
-    with open('members.csv', 'w', encoding='UTF-8') as f:
-        writer = csv.writer(f, delimiter=',', lineterminator='\n')
-        writer.writerow(['username', 'user_id', 'access_hash', 'first_name', 
-                         'last_name', 'group_name', 'group_id'])
-        
-        for user in all_members:
-            # print(dir(target_group))
-            # break
-            writer.writerow([user.username, user.id, 
-                             user.access_hash, user.first_name,
-                             user.last_name, target_group.username, target_group.id])
-    print('Members scraped successfully')
+    print([name.username for name in all_members])
 
 with client:
     client.loop.run_until_complete(main())
-
-# # me = client.get_me()
-# # print(me)
