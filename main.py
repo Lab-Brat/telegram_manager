4b7351c7eb845b64e7ed01ec819538dfa66034bd
Lab-Brat <xooVoox@protonmail.com>
2022-04-06 18:34:09 +0300

    add create group and add users to it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from telethon import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel
+from telethon.tl.functions.channels import CreateChannelRequest
+from telethon.tl.functions.channels import InviteToChannelRequest
 from pprint import pprint
 import time, os, sys, csv, configparser
 
@@ -15,8 +17,16 @@ find_in_group = config['USERS']['userlist'].split(',')
 client = TelegramClient(phone, api_id, api_hash)
 
 async def main():
+
     chats = []
     groups = []
+    gr_title = 'test_grp_2'
+
+    # add group
+    new_group = await client(CreateChannelRequest(
+        title=gr_title, 
+        about='testing api', 
+        megagroup=True))
 
     result = await client(GetDialogsRequest(
                 offset_date=None,
@@ -27,11 +37,18 @@ async def main():
     chats.extend(result.chats)
 
     for chat in chats:
+        # print(chat.title)
         try:
             if chat.megagroup == True:
                 groups.append(chat)
         except:
             continue
+    
+    for group in groups:
+        # print(group.title)
+        if group.title == gr_title:
+            print("===========================")
+            dest_group = group
 
     print('Choose a group to scrape: ')
     for i, group in enumerate(groups):
@@ -48,6 +65,8 @@ async def main():
     time.sleep(1)
 
     target_group = groups[int(gr_index)]
+    print('-----------------')
+    print(type(target_group), type(dest_group))
 
     print('Fetching Members...')
     all_members = []
@@ -59,6 +78,12 @@ async def main():
         if user in find_in_group:
             found_in_group.append(user)
     print(found_in_group)
+
+    # target_group_entity = InputPeerChannel(new_group.id,new_group.access_hash)
+
+    # add users to group
+    await client(InviteToChannelRequest(dest_group.title, found_in_group))
+
 
 with client:
     client.loop.run_until_complete(main())
